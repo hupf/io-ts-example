ecd40862ff176cb7382f9a5c70f601b4b8dedb0e
Mathis Hofer <hofer@puzzle.ch>
2019-08-15 16:26:52 +0200

    Add a User model and service

---
+++ src/app/user.model.ts
@@ -0,0 +1,10 @@
+import * as t from "io-ts";
+
+const User = t.type({
+  id: t.number,
+  name: t.string,
+  username: t.string,
+  email: t.string
+});
+type User = t.TypeOf<typeof User>;
+export { User };
